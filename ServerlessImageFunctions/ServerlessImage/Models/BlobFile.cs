f8d4b54ffe812bad295bbea78b740df1399d7845
Michał Chęciński <michal.checinski@outlook.com>
2019-02-23 16:38:27 +0100

    Add all accepted photos

---
+++ ServerlessImageFunctions/ServerlessImage/Models/BlobFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessImage.Models
+{
+    public class BlobFile
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+
+        public BlobFile(string url, string name)
+        {
+            Url = url;
+            Name = name;
+        }
+    }
+}
